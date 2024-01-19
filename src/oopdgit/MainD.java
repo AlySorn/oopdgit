ce5ebbf7feba3123d89375898d3e5e8bd51701ff
Aly <sornaly854@gmail.com>
2024-01-19 14:51:29 +0700

    test commit2

--- src/oopdgit/MainD.java
+++ src/oopdgit/MainD.java
@@ -5,6 +5,7 @@ public class MainD {
 	public static void main(String[] args) {
 		System.out.println("Hello Aly");
 		System.out.println("Today you did a great job!");
+		System.out.println("Keep going girl!");
 
 	}
 
